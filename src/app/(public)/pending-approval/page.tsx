08f1e6c532414452c0ab001b8942752471e8d91a
laibazahid085 <laiba140308@gmail.com>
2026-07-29 23:10:14 +0500

    delete

---
+++ src/app/(public)/pending-approval/page.tsx
@@ -0,0 +1,8 @@
+export default function PendingApprovalPage() {
+  return (
+    <div className="p-6">
+      <h1>Pending Approval</h1>
+      <p>Your account is pending approval.</p>
+    </div>
+  );
+}
